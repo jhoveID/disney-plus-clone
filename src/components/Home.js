632825b910d2b9b1a526a538f8849669f312659e
Henra <76639525+jhoveID@users.noreply.github.com>
2022-09-03 15:39:29 +0700

    header

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
-
+import styled from 'styled-components';
 function Home() {
-  return <div>Home</div>;
+  return <Container>Home</Container>;
 }
 
 export default Home;
+
+const Container = styled.main`
+  min-height: calc(100vh-70px);
+  padding: 0 calc(3.5vw +5px);
+`;
