b26824c1088015fb23198416ebd8ed49d1727f87
Henra <76639525+jhoveID@users.noreply.github.com>
2022-09-10 13:41:56 +0700

    ADD Movies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import Header from './components/Header';
 import Home from './components/Home';
+import { 
+        BrowserRouter as Router,
+        Switch,
+        Routes, 
+        Route,
+        Link } from "react-router-dom";
 import './App.css';
+import Detail from './components/Detail';
+import { RouteSharp } from '@mui/icons-material';
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <Home />
+        <Router>
+            <Header />
+             <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="detail" element={<Detail />} />
+      </Routes>
+        </Router>
+    
+      
     </div>
   );
 }
 
 export default App;
+
+
